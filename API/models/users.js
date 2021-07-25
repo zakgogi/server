c3ab9c536c44a7da75252b0e70052087a9504559
Zak Gogi <zakgogi1@gmail.com>
2021-07-25 23:51:40 +0100

    updated a route for viewing all habbits based on userid

--- API/models/users.js
+++ API/models/users.js
@@ -19,6 +19,5 @@ module.exports = class User{
             }
         })
     };
-
 };
 
--- API/routes/habits.js
+++ API/routes/habits.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const habitsController = require('../controllers/habits')
+const habitsController = require('../controllers/habits');
+const { route } = require('./users');
 
 // router.get('/', authorsController.index);
 router.get('/', habitsController.show);
+router.get('/:id', habitsController.showJoin);
 
 module.exports = router;
 
