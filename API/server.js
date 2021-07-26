66067eb3a4337a44ba1d88874e86143609c4b714
Zak Gogi <zakgogi1@gmail.com>
2021-07-26 12:28:46 +0100

    added auth routing for login and registration, bcrypt only

--- API/server.js
+++ API/server.js
@@ -8,10 +8,11 @@ server.use(express.json());
 // const authRoutes = require('./routes/auth');
 const habitsRoutes = require('./routes/habits');
 const usersRoutes = require('./routes/users');
-
+const authRoutes = require('./routes/auth');
 // server.use('/auth', authRoutes);
 server.use('/habits', habitsRoutes);
-server.use('/users', usersRoutes)
+server.use('/users', usersRoutes);
+server.use('/auth', authRoutes);
 
 server.get('/', (req, res) => res.send('Welcome to team brogrammers'));
 
