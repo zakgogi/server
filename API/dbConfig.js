e6eb5a6e689a844c27a9a103bf8572577e80d22b
Zak Gogi <zakgogi1@gmail.com>
2021-07-28 11:35:12 +0100

    switched back heroku pooling

--- API/dbConfig.js
+++ API/dbConfig.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+//const pool = new Pool();
 
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = pool;
