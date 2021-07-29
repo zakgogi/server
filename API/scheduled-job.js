6e71810e671709406f3da6d53b09e417f4edc4f6
Zak Gogi <zakgogi1@gmail.com>
2021-07-29 09:45:03 +0100

    moved streak check into scheduled job

--- API/scheduled-job.js
+++ API/scheduled-job.js
@@ -1,4 +1,20 @@
-function sayHello() {
-    console.log('Hello');
-}
-sayHello();
+const db = require('./dbConfig');
+
+function streakCheck(){
+    return new Promise(async(resolve, reject) => {
+        try {
+            const result = await db.query('SELECT id, frequency_day, times_completed FROM Habits;');
+            result.rows.forEach(async r => {
+                const timeCompleteUpdate = await db.query('UPDATE habits SET times_completed = 0 WHERE id = $1;', [r.id]);
+                if (r.frequency_day !== r.times_completed){
+                    const update = await db.query('UPDATE habits SET streak = 0 WHERE id = $1;', [r.id]);
+                };
+            });
+            resolve('Successfully set/reset streak');
+        } catch (err) {
+            reject('Cannot find streak');
+        }
+    })
+};
+
+streakCheck();
